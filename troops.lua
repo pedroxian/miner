d7df71ab241bc4aa8af344f8b166d97144be43b4
pedroxian <44950875+pedroxian@users.noreply.github.com>
2021-06-29 18:18:19 +0200

    Add files via upload

---
+++ troops.lua
@@ -0,0 +1,18 @@
+--lua 5.3.5 
+{
+
+  {print. eval 4567;12 unit}{space.time}EVE
+  Lua.script echo/navy coil.bit=hex.z
+  {
+    maschine.x64(echo) - ring.mod #selektor
+    (
+      press.fantime;pulse/rigs 
+    {
+      crime.want g.bride to replay
+    }
+    ){
+      express.holder
+    }
+  }(involve-clarity)
+
+}end.class
